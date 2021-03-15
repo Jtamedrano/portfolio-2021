7e5480cd41bfa1910b185fbc2729c989151b3f73
Jesse Medrano <Jtamedrano@gmail.com>
2021-03-15 07:12:33 -0700

    close gallary btn

--- src/components/BlackOverlay/Gallary.tsx
+++ src/components/BlackOverlay/Gallary.tsx
@@ -1,12 +1,30 @@
+import CloseIcon from "@material-ui/icons/Close";
+import { MouseEvent, useContext } from "react";
+import { PageContext } from "../../PageContextProvider";
+
 interface GallaryViewProps {
   images: string[];
 }
 
 const Gallary = ({ images }: GallaryViewProps): JSX.Element => {
-  console.log(images);
+  const ctx = useContext(PageContext);
+
+  const handleClose = (e: MouseEvent) => {
+    e.preventDefault();
+    if (!!ctx.setModalState) ctx.setModalState(false);
+  };
+
   return (
     <>
       <div className="gallary-container" onClick={(e) => e.stopPropagation()}>
+        <div className="top-gallary-bar">
+          <CloseIcon
+            color="action"
+            fontSize="large"
+            className="close-btn"
+            onClick={handleClose}
+          />
+        </div>
         <div className="imageWrapper">
           <img src={`/project-images/${images[0]}`} alt="" />
         </div>
