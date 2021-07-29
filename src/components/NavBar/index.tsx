dbafa22d06b6beb590179b58fb73ca92e33aec66
jtamedrano <jtamedrano@gmail.com>
2021-07-29 14:18:15 -0700

    nav default tab goes with routing

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -14,14 +14,18 @@ const index = (props: Props) => {
   return (
     <Space direction="horizontal">
       <div className="logo">Jesse Medrano</div>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" onClick={() => handleGoTo('')}>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={[`${history.location.pathname}`]}
+      >
+        <Menu.Item key="/" onClick={() => handleGoTo('')}>
           Home
         </Menu.Item>
-        <Menu.Item key="2" onClick={() => handleGoTo('about-me')}>
+        <Menu.Item key="/about-me" onClick={() => handleGoTo('about-me')}>
           About Me
         </Menu.Item>
-        <Menu.Item key="3" onClick={() => handleGoTo('projects')}>
+        <Menu.Item key="/projects" onClick={() => handleGoTo('projects')}>
           Projects
         </Menu.Item>
       </Menu>
