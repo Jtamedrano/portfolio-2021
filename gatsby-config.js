a037caa3065b0c15f8020ad5ff5b2ce35f473871
jtamedrano <jtamedrano@gmail.com>
2021-08-31 16:27:28 -0700

    manifest and favicos added

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,35 @@ module.exports = {
     siteUrl: "https://jtamedrano.com",
     title: "Jesse Medrano",
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jesse Medrano - Developer`,
+        short_name: `Jesse Medrano`,
+        description:
+          "Jesse Medrano, Software Engineer & Web Developer based out of Southern California",
+        start_url: `/`,
+        lang: "en",
+        icon: "./src/images/favicon-32x32.png",
+        icons: [
+          {
+            src: "./src/images/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./src/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    },
+  ],
 };
