3b3f606e67fadc1d96a83f6ae7c021a1ee87baa4
Jesse Medrano <Jtamedrano@gmail.com>
2025-01-25 13:29:31 -0800

    request bundle form

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -16,9 +16,9 @@ export const Banner: React.FC<React.PropsWithChildren<BannerProps>> = ({
     <section
       className={cs("bg-gradient-to-br from-blue-300 to-blue-400", className)}
     >
-      <div className="container flex flex-col lg:flex-row justify-center items-center self-stretch mx-auto py-4 px-4 xl:px-0 gap-12 overflow-hidden">
+      <div className="container flex flex-col lg:flex-row justify-center items-center self-stretch mx-auto py-4 px-4 xl:px-0 gap-x-12 overflow-hidden">
         <div className="flex-1 space-y-4">
-          <h2 className="font-bold tracking-tight uppercase text-pretty">
+          <h2 className="font-bold tracking-tight uppercase text-pretty text-center lg:text-left">
             {title}
           </h2>
           {description && (
