e7b69858fcd1faea6d2effed1d02a200047129c5
Jesse Medrano <Jtamedrano@gmail.com>
2025-02-09 13:23:33 -0800

    Release/feb (#20)
    * task/update-home-copy
    
    * local seo and attempt to size images better
    
    * add rancho
    
    * add clarity
    
    * use clarity pkg

--- src/components/Analytics/GoogleAnalytics.tsx
+++ src/components/Analytics/GoogleAnalytics.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Clarity from "@microsoft/clarity";
 import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 import { useEffect } from "react";
@@ -8,8 +9,10 @@ export const GoogleAnalytics = () => {
   const pathname = usePathname();
   const gamid = process.env.NEXT_PUBLIC_GA_MID;
   const isDev = process.env.NODE_ENV === "development";
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID;
 
   useEffect(() => {
+    if (!window) return;
     if (window?.gtag && gamid && !isDev) {
       window.gtag("config", gamid, {
         page_path: pathname,
@@ -17,7 +20,32 @@ export const GoogleAnalytics = () => {
         page_title: document.title,
       });
     }
-  }, [pathname]);
+    if (clarityId && !isDev) {
+      // (function (
+      //   c: { [key: string]: any },
+      //   l,
+      //   a,
+      //   r,
+      //   i,
+      //   t?: HTMLScriptElement,
+      //   y?: HTMLElement
+      // ) {
+      //   c[a] =
+      //     c[a] ||
+      //     function () {
+      //       (c[a].q = c[a].q || []).push(arguments);
+      //     };
+      //   t = l.createElement(r) as HTMLScriptElement;
+      //   t.async = true;
+      //   t.src = "https://www.clarity.ms/tag/" + i;
+      //   y = l.getElementsByTagName(r)[0] as HTMLElement;
+      //   if (y?.parentNode) {
+      //     y.parentNode.insertBefore(t, y);
+      //   }
+      // })(window, document, "clarity", "script", clarityId);
+      Clarity.init(clarityId);
+    }
+  }, [pathname, gamid, isDev, clarityId]);
 
   return (
     <head>
